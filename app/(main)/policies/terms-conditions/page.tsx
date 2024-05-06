c5fa27d013bbe93bf1d0da060b1f0071dc75d9bf
Harvey Yemm <hjyemm@icloud.com>
2024-05-06 15:48:31 +0100

    fixed vercel errors

--- app/(main)/policies/terms-conditions/page.tsx
+++ app/(main)/policies/terms-conditions/page.tsx
@@ -8,51 +8,50 @@ const Terms = () => {
           RYZENS Wholesale Terms &amp; Conditions
         </h1>
         <p>
-          These Terms and Conditions, along with any Agreements, are
-          collectively referred to as: The terms &quot;customer&quot;
+          These Terms and Conditions along with any Agreements are collectively referred to as: The terms &quot;customer&quot;
           &quot;you&quot; and &quot;your&quot; refer to you the individual
-          browsing our website and/or making a purchase. Our company, RYZENS
-          Wholesale LTD, is referred to as &quot;The Company,&quot;
-          &quot;Ourselves&quot; &quot;We&quot; and &quot;Us.&quot;
+          browsing our website and/or making a purchase. Our company RYZENS
+          Wholesale LTD is referred to as &quot;The Company&quot;
+          &quot;Ourselves&quot; &quot;We&quot; and &quot;Us&quot;
         </p>
         <p>
-          All terms refer to the offer, acceptance, and consideration of payment
-          necessary to undertake the process of our assistance to the Customer.
+          All terms refer to the offer acceptance and consideration of payment
+          necessary to undertake the process of our assistance to the Customer
         </p>
         <h2 className="text-xl font-bold mt-4 mb-2">Export Terms</h2>
         <ul className="list-disc ml-6">
-          <li>Minimum export spend is £50.</li>
+          <li>Minimum export spend is £50</li>
           <li>
-            If goods are to be VAT exempt, they must be collected by a reputable
-            shipper, who must provide us with a Collection Note and Bill of
-            Lading for Air Freight or Shipment Certificate for sea freight.
+            If goods are to be VAT exempt they must be collected by a reputable
+            shipper who must provide us with a Collection Note and Bill of
+            Lading for Air Freight or Shipment Certificate for sea freight
           </li>
           <li>
-            We accept cash, bank transfer &amp; debit cards only as methods of
-            payment.
+            We accept cash bank transfer &amp; debit cards only as methods of
+            payment
           </li>
           <li>
             Bank transfer funds must reflect in our bank account before we
-            release goods to you.
+            release goods to you
           </li>
           <li>
-            We do not accept UK credit cards, international debit or credit
-            cards, or cheques.
+            We do not accept UK credit cards international debit or credit
+            cards or cheques
           </li>
           <li>
-            Goods must be collected by the shipper within 2 weeks of purchase.
+            Goods must be collected by the shipper within 2 weeks of purchase
           </li>
           <li>
-            Full payment must be received by RYZENS Wholesale Ltd. before goods
-            can be released to Freight Agent.
+            Full payment must be received by RYZENS Wholesale Ltd before goods
+            can be released to Freight Agent
           </li>
         </ul>
         <p>
           These terms and conditions are part of the Agreement between the
-          Customer and ourselves. Your access to this website and/or undertaking
-          a sale indicates your understanding, agreement, and acceptance of the
-          full Terms and Conditions contained herein. Your statutory Consumer
-          Rights are unaffected.
+          Customer and ourselves Your access to this website and/or undertaking
+          a sale indicates your understanding agreement and acceptance of the
+          full Terms and Conditions contained herein Your statutory Consumer
+          Rights are unaffected
         </p>
       </div>
     </>
